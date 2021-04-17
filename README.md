84945cc4b48a63df870ea80b46fb48a748634904
dodo325 <dodo101000101@gmail.com>
2021-04-17 04:50:26 +0300

    feat(API): add models

--- README.md
+++ README.md
@@ -1,8 +1,26 @@
 # Word-Vectors-API
 FastAPI for gensim nlp models
 
-Запуск через 
+
+## Запуск:
+0. Устанавливаем зависимости:
+```bash
+    pip install -r requirements.txt
+```
+
+1. Качаем модель
+```bash
+    wget http://vectors.nlpl.eu/repository/20/187.zip -P models
+```
+
+2. Распоковываем:
+```bash
+    unzip 214.zip -d geowac_tokens_none_fasttextskipgram_300_5_2020
+```
+
+3. Запуск через 
 
 ```
     uvicorn main:app --reload
 ```
+
--- main.py
+++ main.py
@@ -1,13 +1,40 @@
 from gensim.models import KeyedVectors
 from fastapi import FastAPI
+from pathlib import Path
+from typing import Optional
+
 
 app = FastAPI()
-m_file = "models/geowac_tokens_none_fasttextskipgram_300_5_2020/model.model"
-
 items = {}
+
+
+def get_models_dirs(base_dir = "models"):
+    path = Path(base_dir)
+    return [x for x in path.iterdir() if x.is_dir()]
+
+
+def get_model_by_name(model_name: str) -> Optional[KeyedVectors]:
+    models_dirs = get_models_dirs()
+    for model_dir in models_dirs:
+        if model_dir.name == model_name:
+            model_file = model_dir / "model.model"
+            return KeyedVectors.load(str(model_file), mmap='r')
+
+
 @app.on_event("startup")
 async def startup_event():
-    items["model"] = KeyedVectors.load(m_file, mmap='r')
+    models_dirs = get_models_dirs()
+    model_file = models_dirs[0] / "model.model"
+    items["model"] = KeyedVectors.load(str(model_file), mmap='r')
+    items["model_name"] = models_dirs[0].name
+
+
+@app.get("/models/")
+async def read_root():
+    return [
+            str(x.name) for x in get_models_dirs()
+        ]
+
 
 @app.get("/")
 async def read_root():
@@ -16,4 +43,9 @@ async def read_root():
 @app.get("/similarity/{word_1}/{word_2}")
 async def get_similarity(word_1: str, word_2: str):
     val = float(items["model"].similarity(word_1, word_2))
-    return {"value": val}
+    return {
+        "value": val,
+        "word_1": word_1,
+        "word_2": word_2,
+        "model_name": items["model_name"]
+        }
